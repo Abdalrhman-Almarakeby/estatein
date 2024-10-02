471cbd13024fbed68f4a953ef30299cb2813ba74
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2024-10-02 14:15:04 +0300

    refactor: clean up dashboard by removing test code

--- src/app/dashboard/(dashboard)/page.tsx
+++ src/app/dashboard/(dashboard)/page.tsx
@@ -1,25 +1,3 @@
-"use client";
-
-import { signIn, signOut, useSession } from "next-auth/react";
-
 export default function Page() {
-  const session = useSession();
-
-  if (session.status === "loading") {
-    return "loading...";
-  }
-
-  if (session.status === "unauthenticated") {
-    return (
-      <div>
-        unauthenticated <button onClick={() => signIn()}>Signin</button>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      Dashboard <button onClick={() => signOut()}>Sign out</button>
-    </div>
-  );
+  return <div>Dashboard</div>;
 }
