9153302aaeef460700a95669d63a2ca713b9f80a
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2024-10-04 07:10:28 +0300

    feat(dialog): add Dialog component via shadcn/ui CLI, and refactor it into multiple files
    - Added Dialog component using shadcn/ui CLI
    - Split the component into separate files
    - Updated styles to ensure consistency with other components

---
+++ src/components/ui/dialog/dialog-content.tsx
@@ -0,0 +1,31 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { DialogOverlay } from "./dialog-overlay";
+import { DialogPortal } from "./dialog-portal";
+
+export const DialogContent = forwardRef<
+  ElementRef<typeof DialogPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <DialogPortal>
+    <DialogOverlay />
+    <DialogPrimitive.Content
+      ref={ref}
+      className={cn(
+        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+      <DialogPrimitive.Close className="absolute right-3 top-3 rounded-sm p-1 opacity-70 ring-offset-white transition-opacity hover:opacity-100 disabled:pointer-events-none">
+        <X className="size-5" />
+        <span className="sr-only">Close</span>
+      </DialogPrimitive.Close>
+    </DialogPrimitive.Content>
+  </DialogPortal>
+));
+
+DialogContent.displayName = DialogPrimitive.Content.displayName;
