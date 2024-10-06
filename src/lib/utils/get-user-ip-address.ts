21f7b712bc01114955642f29f2413dba8105e318
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2024-10-06 17:05:17 +0300

    feat: add getUserIpAddress function to retrieve user IP address

---
+++ src/lib/utils/get-user-ip-address.ts
@@ -0,0 +1,14 @@
+import { headers } from "next/headers";
+
+const FALLBACK_IP_ADDRESS = "0.0.0.0";
+
+export function getUserIpAddress() {
+  const headersList = headers();
+  const forwardedFor = headersList.get("x-forwarded-for");
+
+  if (forwardedFor) {
+    return forwardedFor.split(",")[0] ?? FALLBACK_IP_ADDRESS;
+  }
+
+  return headersList.get("x-real-ip") ?? FALLBACK_IP_ADDRESS;
+}
