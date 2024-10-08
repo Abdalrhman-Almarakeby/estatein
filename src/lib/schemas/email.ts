f1d2cd02e7432a0d103e166a42290df00f1fc254
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2024-10-08 23:29:12 +0300

    fix: typo in emailSchema

--- src/lib/schemas/email.ts
+++ src/lib/schemas/email.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 export const emailSchema = z.object({
   email: z
     .string()
-    .min(1, "PLease enter you email")
-    .email("PLease enter a valid email"),
+    .min(1, "Please enter you email")
+    .email("Please enter a valid email"),
 });
 
 export type Email = z.infer<typeof emailSchema>;
