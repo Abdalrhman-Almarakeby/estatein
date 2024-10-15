d4662ee5b65a9fc5b8bc3f63a5dc9c717a3e53c3
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2024-10-15 07:16:51 +0300

    refactor: update inquiry referral source

--- src/constant/referral-source.ts
+++ src/constant/referral-source.ts
@@ -1,35 +1,8 @@
 export const REFERRAL_SOURCE = [
-  {
-    label: "Social media",
-    value: "SOCIAL_MEDIA",
-  },
-  {
-    label: "ADs",
-    value: "ADS",
-  },
-  {
-    label: "Search",
-    value: "SEARCH",
-  },
-  {
-    label: "Business Partner",
-    value: "BUSINESS_PARTNER",
-  },
-  {
-    label: "Friend/family_member",
-    value: "FRIEND_FAMILY_MEMBER",
-  },
-  {
-    label: "other",
-    value: "OTHER",
-  },
-] as const;
-
-export const REFERRAL_SOURCE_VALUES = [
   "SOCIAL_MEDIA",
   "ADS",
   "SEARCH",
   "BUSINESS_PARTNER",
-  "FRIEND_FAMILY_MEMBER",
+  "FRIEND_OR_FAMILY_MEMBER",
   "OTHER",
 ] as const;
