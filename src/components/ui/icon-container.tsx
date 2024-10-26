e4172c8a67fcdcfcd54fe160e8f1b050d534b701
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2024-10-26 11:54:20 +0300

    refactor: update icon-container to take icons props

--- src/components/ui/icon-container.tsx
+++ src/components/ui/icon-container.tsx
@@ -1,21 +1,30 @@
+import { SVGProps } from "react";
 import { SVGcomponent } from "@/types";
-import { cn } from "@/lib/utils";
+import { cn, omit } from "@/lib/utils";
 import IconContainerSVG from "@/assets/icon-container.svg";
 
 type IconContainerProps = {
   Icon: SVGcomponent;
+  iconProps?: SVGProps<SVGSVGElement>;
   className?: string;
 };
 
 export function IconContainer({
   Icon,
   className,
+  iconProps,
   ...props
 }: IconContainerProps) {
   return (
     <span className={cn("relative", className)} aria-hidden="true" {...props}>
       <IconContainerSVG className="size-12 animate-[spin_10s_linear]" />
-      <Icon className="absolute right-1/2 top-1/2 size-5 -translate-y-1/2 translate-x-1/2 stroke-purple-light" />
+      <Icon
+        {...(iconProps ? { ...omit(iconProps, "className") } : {})}
+        className={cn(
+          "absolute right-1/2 top-1/2 size-5 -translate-y-1/2 translate-x-1/2 stroke-purple-light",
+          iconProps?.className,
+        )}
+      />
     </span>
   );
 }
