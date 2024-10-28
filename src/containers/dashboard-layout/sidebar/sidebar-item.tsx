5144fa6fd99ee19959b5bd17609278d53218145e
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2024-10-28 14:50:57 +0300

    fix: dashboard sidebar

--- src/containers/dashboard-layout/sidebar/sidebar-item.tsx
+++ src/containers/dashboard-layout/sidebar/sidebar-item.tsx
@@ -10,12 +10,13 @@ type SidebarItemProps = {
 };
 
 export function SidebarItem({ href, icon, label }: SidebarItemProps) {
-  const { isSidebarOpen } = useDashboardSidebar();
+  const { isSidebarOpen, setIsSidebarOpen } = useDashboardSidebar();
 
   return (
     <Link
       tabIndex={isSidebarOpen ? 0 : -1}
       href={href}
+      onClick={() => setIsSidebarOpen(false)}
       className="flex items-center gap-3 rounded-md p-2 text-gray-light !no-underline transition-colors"
     >
       {icon}
