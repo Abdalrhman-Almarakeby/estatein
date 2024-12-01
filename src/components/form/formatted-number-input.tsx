3a795b1910fcf1698f8afce7777d180511147a86
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2024-12-01 16:09:50 +0200

    feat: add FormattedNumberInput component

---
+++ src/components/form/formatted-number-input.tsx
@@ -0,0 +1,52 @@
+import {
+  Control,
+  FieldPath,
+  FieldValues,
+  useController,
+} from "react-hook-form";
+import { NumericFormat, NumericFormatProps } from "react-number-format";
+import { Input } from "./input";
+
+type FormattedNumberInputProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> = Omit<NumericFormatProps, "onChange" | "name"> & {
+  control: Control<TFieldValues>;
+  name: TName;
+  isCurrency?: boolean;
+};
+
+export function FormattedNumberInput<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+  control,
+  name,
+  isCurrency = false,
+  ...props
+}: FormattedNumberInputProps<TFieldValues, TName>) {
+  const {
+    field: { onChange, value, ref: fieldRef },
+  } = useController({
+    name,
+    control,
+  });
+
+  return (
+    <NumericFormat
+      customInput={Input}
+      thousandSeparator=","
+      decimalSeparator="."
+      decimalScale={0}
+      prefix={isCurrency ? "$" : ""}
+      allowNegative={false}
+      onValueChange={(values) => {
+        onChange(values.floatValue);
+      }}
+      value={value}
+      valueIsNumericString={true}
+      getInputRef={fieldRef}
+      {...props}
+    />
+  );
+}
