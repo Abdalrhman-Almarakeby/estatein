13ee30436d2ce3b994484cfaf33ac74bcdfd67b9
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-02-03 03:33:15 +0200

    refactor: simplify getBaseUrl function

--- src/lib/utils/get-base-url.ts
+++ src/lib/utils/get-base-url.ts
@@ -1,17 +1,11 @@
 import { env } from "@/lib/env";
 
-function getProtocol() {
-  if (env.NEXT_PUBLIC_VERCEL_URL) {
-    return "https://";
-  }
-
-  return "http://";
-}
-
 export function getBaseUrl() {
-  if (env.NEXT_PUBLIC_VERCEL_URL) {
-    return `${getProtocol()}${env.NEXT_PUBLIC_VERCEL_URL}`;
-  }
+  const protocol = env.NEXT_PUBLIC_VERCEL_URL ? "https://" : "http://";
+  const baseUrl =
+    env.NEXT_PUBLIC_VERCEL_URL ||
+    env.NEXT_PUBLIC_BASE_URL ||
+    "http://localhost:3000";
 
-  return env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000";
+  return `${protocol}${baseUrl}`;
 }
