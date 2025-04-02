bd97bea8e10e28935fbde22324dfe778aa22ce31
Abdalrhman Almarakeby <contact@abdalrhman.me>
2025-04-02 20:31:30 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -8,13 +8,13 @@ export type ScrollDirection = "up" | "down";
 
 export type SVGComponent = FunctionComponent<SVGProps<SVGSVGElement>>;
 
-export type WithCaptcha<T extends Object> = T & Captcha;
+export type WithCaptcha<T extends object> = T & Captcha;
 
-export type StrictRequired<T extends Object> = {
+export type StrictRequired<T extends object> = {
   [K in keyof T]-?: NonNullable<T[K]>;
 };
 
-export type StrictOmit<T extends Object, K extends keyof T> = Pick<
+export type StrictOmit<T extends object, K extends keyof T> = Pick<
   T,
   Exclude<keyof T, K>
 >;
