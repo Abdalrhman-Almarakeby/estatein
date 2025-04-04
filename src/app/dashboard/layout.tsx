722dda0acf18542f550e27b802fdd396dadf4e01
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-04-04 13:15:52 +0200

    refactor: remove SessionProvider from Layout component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { ReactNode } from "react";
-import { SessionProvider } from "next-auth/react";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 export default function Layout({ children }: LayoutProps) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return children;
 }
