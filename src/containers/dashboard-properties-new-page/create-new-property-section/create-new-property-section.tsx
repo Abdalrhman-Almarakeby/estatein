78c746839487f71ccd47f4c521d7efa19962f15c
Abdalrhman Almarakeby <contact@abdalrhman.me>
2025-05-24 19:58:22 +0300

    feat: add back navigation link to properties dashboard in create new property section

--- src/containers/dashboard-properties-new-page/create-new-property-section/create-new-property-section.tsx
+++ src/containers/dashboard-properties-new-page/create-new-property-section/create-new-property-section.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
+import { ArrowLeft } from "lucide-react";
 import { PropertyCreatedSuccess } from "@/containers/dashboard-properties-new-page/property-form";
 import { PropertyForm } from "@/containers/dashboard-properties-new-page/property-form/property-form";
 import { PropertyData } from "@/lib/schemas";
@@ -29,7 +31,14 @@ export function CreateNewPropertySection() {
       onCreateNewProperty={() => setCreatedProperty(null)}
     />
   ) : (
-    <div className="container mx-auto py-10">
+    <div className="container mx-auto space-y-10 py-10">
+      <Link
+        href="/dashboard/properties"
+        className="btn btn-primary flex w-fit items-center gap-3 rounded-md px-2.5 py-1.5"
+      >
+        <ArrowLeft className="size-5" />
+        Back to Properties
+      </Link>
       <h1 className="mb-8 text-3xl font-bold">Add New Property</h1>
       <PropertyForm
         onSubmit={handleSubmit}
