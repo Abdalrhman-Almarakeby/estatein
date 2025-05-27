4620cfd4d08081a6597eb94b363dd24362e19ba8
Abdalrhman Almarakeby <contact@abdalrhman.me>
2025-05-27 11:45:13 +0300

    feat: conditionally render admin link in account settings based on user role

--- src/app/dashboard/(dashboard)/account/page.tsx
+++ src/app/dashboard/(dashboard)/account/page.tsx
@@ -14,13 +14,15 @@ export default async function Page() {
     <div className="container min-h-screen py-12">
       <div className="mb-12 flex flex-col justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="text-3xl font-bold">Account Settings</h1>
-        <Link
-          href="/dashboard/admin"
-          className="btn-sm btn-primary flex items-center gap-3 rounded-md px-4 py-2 text-base font-medium"
-        >
-          <Shield className="stroke-3 size-5" />
-          Admin Page
-        </Link>
+        {user.role === "ADMIN" && (
+          <Link
+            href="/dashboard/admin"
+            className="btn-sm btn-primary flex items-center gap-3 rounded-md px-4 py-2 text-base font-medium"
+          >
+            <Shield className="stroke-3 size-5" />
+            Admin Page
+          </Link>
+        )}
       </div>
       <div className="space-y-6">
         <AccountInformation email={user.email} username={user.username} />
